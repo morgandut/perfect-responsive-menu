5ef8aab59bdd59d8a58c8782d4f5ce7736247c3e
dutreix <morgandutreix@hotmail.fr>
2020-03-30 15:37:17 +0200

    firstcommit

--- navigation.js
+++ navigation.js
@@ -1,6 +1,6 @@
 const toggleMenu = document.querySelector('.navigation button');
 const menu = document.querySelector('.navigation ul');
-
+document.getElementById("menu").className = "noscroll"
 toggleMenu.addEventListener('click', function() {
 	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
 	toggleMenu.setAttribute('aria-expanded', !open);
